bd6c74e75791107e5e3642bd44c67c9c9ebb843c
Glen Souza <glenster75@gmail.com>
2023-09-04 23:21:34 -0700

    scores

---
+++ GitHubPages/Models/Score.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubPages.Models
+{
+    public class Score
+    {
+        public string CategoryName { get; set; } = string.Empty;
+        public int Correct { get; set; }
+        public int Total { get; set; }
+    }
+}
